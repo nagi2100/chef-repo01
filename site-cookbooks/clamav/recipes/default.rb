fe8c4bdc59b92ef074ce4238e237653ab1e3ad7d
ookita <ookita@knifesolo00.(none)>
2013-11-11 14:43:50 +0900

    ookita cookbook commit

--- site-cookbooks/clamav/recipes/default.rb
+++ site-cookbooks/clamav/recipes/default.rb
@@ -6,65 +6,52 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-#filename = "epel-release-6-8.noarch.rpm"
-#file_checksum = "e5ed9ecf22d0c4279e92075a64c757ad2b38049bcf5c16c4f2b75d5f6860dc0d"
 
-#cookbook_file "/tmp/#{filename}" do
-#  source "#{filename}"
-#  checksum "#{file_checksum}"
-#end
-
-#package "epel" do
-#  action :install
-#  provider Chef::Provider::Package::Rpm
-#  source "/tmp/#{filename}"
-#end
-
-include_recipe 'yum::epel'
-
+#clamdインストール
 %w{clamd}.each do |package_name|
   package package_name do
     action :install
   end
 end
 
-cron "cron_freshclam" do
-  user "root"
-  minute "00"
-  hour "3"
-  weekday "2"
-  command %Q!/usr/bin/freshclam --quiet -l /var/log/clamav/freshclam.log!
-  action :create
-end
-
-#cron "freshclam" do
-#  action node.tags.include?('freshclam') ? :create : :delete
+#定義ファイル更新cron設定
+#cron "cron_freshclam" do
+#  user "root"
 #  minute "00"
 #  hour "3"
 #  weekday "2"
-#  user "root"
-#  command %Q{
-#     /usr/bin/freshclam --quiet -l /var/log/clamav/freshclam.log
-#  }
+#  command %Q!/usr/bin/freshclam --quiet -l #{node['clamav']['log_path']}#{node['clamav']['freshclam_log_filename']}!
+#  action :create
 #end
 
+#定期スキャン用スクリプト配置
 template "clamscan_script" do
-  path "/usr/local/bin/clam-full.sh"
+  path "#{node['clamav']['script_path']}clam-full.sh"
   source "clam-full.erb"
   mode 0755
 end
 
+#定期スキャン用cron設定
 cron "cron_clamscan" do
   user "root"
   minute "00"
   hour "3"
   weekday "2"
-  command %Q!/usr/local/bin/clam-full.sh!
+  command %Q!#{node['clamav']['script_path']}clam-full.sh!
   action :create
 end
 
-directory "/var/log/clamav/virus" do
+#隔離ディレクトリ作成
+directory "#{node['clamav']['move_path']}" do
   recursive true
   mode 0755
   action :create
+end
+
+# scan除外ファイルの設置
+template "#{node['clamav']['scan_exclude_config_path']}" do
+  source "clamscan.exclude.erb"
+  owner  "root"
+  group  "root"
+  mode   0644
 end
