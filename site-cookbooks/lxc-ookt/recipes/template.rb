3810a18a460b0ba06d4283fa0c67b07830e00ef8
ookita <ookita@knifesolo00.(none)>
2013-12-04 17:46:11 +0900

    modify cookbook lxc-ookt

--- site-cookbooks/lxc-ookt/recipes/template.rb
+++ site-cookbooks/lxc-ookt/recipes/template.rb
@@ -6,10 +6,29 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+#関係するパッケージのインストール(ubuntuでcentosを動かす場合yumが必要)
+%w{yum}.each do |package_name|
+  package package_name do
+    action :install
+  end
+end
+
+#centosテンプレートの配置
 cookbook_file "lxc-centos" do
   path "/usr/lib/lxc/templates/lxc-centos"
   owner  "root"
   group  "root"
   mode 0755
-  action :create_if_missing
+  action :create
 end
+
+#ubuntuテンプレートの配置
+cookbook_file "lxc-ubuntu" do
+  path "/usr/lib/lxc/templates/lxc-ubuntu"
+  owner  "root"
+  group  "root"
+  mode 0755
+  action :create
+end
+
