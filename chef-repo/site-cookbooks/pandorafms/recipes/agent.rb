88e50ba56630955614ceb10db62f3aa25b37d22c
Yama-Tomo <mazda13bturbo@gmail.com>
2014-01-15 13:52:31 +0900

    add pandora agent

---
+++ chef-repo/site-cookbooks/pandorafms/recipes/agent.rb
@@ -0,0 +1,25 @@
+#
+# Cookbook Name:: pandorafms
+# Recipe:: agent
+#
+# Copyright 2013, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+## plugins ##
+cookbook_file "/etc/pandora/plugins/pandora_df_custom" do
+  source "plugins/pandora_df_custom"
+  owner "pandora"
+  group "root"
+  mode 0755
+end
+
+template "/etc/pandora/pandora_agent.conf" do
+  source "pandora_agent.conf.erb"
+  notifies :restart, 'service[pandora_agent_daemon]'
+end
+
+service "pandora_agent_daemon" do
+  supports :status => true, :restart => true, :start => true
+end
