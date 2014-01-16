befa935fc9e2ee5e58934974a95d07bed39e4c45
Yama-Tomo <mazda13bturbo@gmail.com>
2014-01-16 19:08:45 +0900

    pandora agent yumでインストールするように修正 各nodesのjson修正 mysql replication plugin追加

--- chef-repo/site-cookbooks/pandorafms/recipes/agent.rb
+++ chef-repo/site-cookbooks/pandorafms/recipes/agent.rb
@@ -7,6 +7,10 @@
 # All rights reserved - Do Not Redistribute
 #
 
+package "pandorafms_agent_unix" do
+  action :install
+end
+
 ## plugins ##
 cookbook_file "/etc/pandora/plugins/pandora_df_custom" do
   source "plugins/pandora_df_custom"
@@ -14,6 +18,19 @@ cookbook_file "/etc/pandora/plugins/pandora_df_custom" do
   group "root"
   mode 0755
 end
+cookbook_file "/etc/pandora/plugins/memory_usage" do
+  source "plugins/memory_usage"
+  owner "pandora"
+  group "root"
+  mode 0755
+end
+cookbook_file "/etc/pandora/plugins/mysql_replication_status" do
+  source "plugins/mysql_replication_status"
+  owner "pandora"
+  group "root"
+  mode 0755
+end
+## plugins ##
 
 template "/etc/pandora/pandora_agent.conf" do
   source "pandora_agent.conf.erb"
